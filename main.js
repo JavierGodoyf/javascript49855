6bcc4f3e1bcd90b20c6cf150816884f5e343ad11
JavierGodoyf <javier.godoyf91@gmail.com>
2024-01-27 00:37:29 -0300

    ultimos detalles 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const listaDeTareas = []
+const listaDeTareas = [];
 function nuevaTarea(descripcion) {
     nuevaTarea.contador = (nuevaTarea.contador || 0) + 1;
 
@@ -10,18 +10,18 @@ function nuevaTarea(descripcion) {
         if (this.estado == true)
             alert("La tarea " + this.descripcion + " ha cambiado de estado a completada");
         else
-            alert("la tarea " + this.descripcion + " ha cambiado de estado a pendiente")
+            alert("la tarea " + this.descripcion + " ha cambiado de estado a pendiente");
     }
 }
 function agregarTarea() {
     let descripcion = prompt("Ingresa la descripcion de la tarea");
     if (descripcion === null) {
-        alert("La descripcion no puede estar vacia")
+        alert("La descripcion no puede estar vacia");
         return;
     }
     let tarea = new nuevaTarea(descripcion);
     listaDeTareas.push(tarea);
-    alert("la tarea: " + tarea.descripcion + " ha sido ingresada")
+    alert("la tarea: " + tarea.descripcion + " ha sido ingresada");
 }
 
 function filtrarTareas() {
@@ -42,7 +42,7 @@ function filtrarTareas() {
             case 2:
                 let tareasPendientes = listaDeTareas.filter(tarea => !tarea.estado);
                 if (tareasPendientes.length > 0) {
-                    alert("Tareas pendientes en consola")
+                    alert("Tareas pendientes en consola");
                     console.log("Tareas pendientes:");
                     console.table(tareasPendientes);
                 } else {
@@ -53,7 +53,7 @@ function filtrarTareas() {
                 alert("La opción ingresada no es válida");
         }
     } else {
-        alert("No hay tareas en la lista de tareas")
+        alert("No hay tareas en la lista de tareas");
     }
 }
 
@@ -75,29 +75,29 @@ do {
     opc = parseInt(prompt("Bienvenido al Gestor de tareas \n\nLas listas de tarea aparecen en consola\n\n\n1)Ingresa una tarea\n2)cambiar estado de tarea\n3)filtrar tareas\n4)Salir"));
     switch (opc) {
         case 1:
-            agregarTarea()
+            agregarTarea();
             break;
         case 2:
             if (listaDeTareas.length > 0) {
                 console.table(listaDeTareas);
-                let id = parseInt(prompt("ingrese el ID de la tarea a cambiar"))
+                let id = parseInt(prompt("ingrese el ID de la tarea a cambiar"));
                 if (isNaN(id)) {
-                    alert("valor no admitido")
+                    alert("valor no admitido");
                 } else {
-                    completarTareaPorId(id)
+                    completarTareaPorId(id);
                 }
             } else {
-                alert("no hay tareas en la lista")
+                alert("no hay tareas en la lista");
             }
             break;
         case 3:
             filtrarTareas();
             break;
         case 4:
-            alert("Gracias por usar el gestor de tareas")
+            alert("Gracias por usar el gestor de tareas");
             console.log("Programa terminado");
             break;
         default:
             alert("La opcion ingresa no es valida");
     }
-} while (opc != 4)
+} while (opc != 4);
